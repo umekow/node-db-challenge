04562c9e460b067be460d57a8fcc733d56cbbdb7
umekow <umekolashun@gmail.com>
2019-11-08 11:14:35 -0600

    created table schema

--- data/migrations/20191108103323_projects.js
+++ data/migrations/20191108103323_projects.js
@@ -3,10 +3,23 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('projects', tbl => {
       tbl.increments(); 
+      tbl.string('name', 255).notNullable(); 
+      tbl.string('description', 255); 
+      tbl.boolean('completed').defaultTo(false); 
       
   })
   .createTable('tasks', tbl => {
       tbl.increments(); 
+      tbl.string('description', 255).notNullable(); 
+      tbl.sting('notes', 255).notNullable(); 
+      tbl.boolean('completed').defaultTo(false); 
+      tbl.integer('projects_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE'); 
+      
   })
   .createTable('resources', tbl => {
       tbl.increments(); 
@@ -16,12 +29,35 @@ exports.up = function(knex) {
       tbl.integer('projects_id')
         .unsigned()
         .references('id')
-        .inTable('species')
+        .inTable('projects')
         .onDelete('RESTRICT')
         .onUpdate('CASCADE'); 
   })
+  .createTable('projects_resources', tbl => {
+      tbl.increments(); 
+      //fk for projects
+      tbl.integer('projects_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+
+        //fk for tasks
+        tbl.integer('tasks_id')
+        .unsigned()
+        .references('id')
+        .inTable('tasks')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+  });
+  
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('projects_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects'); 
 };
